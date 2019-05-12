e4953e22351189073e806c349728e781b01059df
Flo <flo.rambur@gmail.com>
2019-05-13 00:51:05 +0200

    Allowed DELETE;

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ Server configuration
             server.all('/*', function(req, res, next) {
                 res.header("Access-Control-Allow-Origin", "*");
                 res.header("Access-Control-Allow-Headers", "Authorization, X-Requested-With, Content-Type, Accept");
-                res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+                res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
                 next();
             });
 
