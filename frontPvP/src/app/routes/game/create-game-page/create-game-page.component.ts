d1dde35a49413fa060211a13827ef9f86fbde1fc
Flo <flo.rambur@gmail.com>
2019-05-13 00:54:40 +0200

    Fixed create game and added control on image;

--- frontPvP/src/app/routes/game/create-game-page/create-game-page.component.ts
+++ frontPvP/src/app/routes/game/create-game-page/create-game-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Router } from "@angular/router";
@@ -14,6 +14,9 @@ import { UtilsService } from '../../../services/utils/utils.service';
 
 export class CreateGamePageComponent implements OnInit {
 
+  // @ViewChild('myFile')
+  // myFile: ElementRef;
+
   public form: FormGroup;
   private imageBase64: string;
 
@@ -24,23 +27,24 @@ export class CreateGamePageComponent implements OnInit {
     private Router: Router
   ) { }
 
-  private initForm = () => {
-    this.form = this.FormBuilder.group({
-      name: [ undefined, Validators.required ],
-      platforms: this.FormBuilder.array([this.createPlatform()]),
-      modes: this.FormBuilder.array([this.createMode()]),
-      rules: this.FormBuilder.array([this.createRule()]),
-      image: this.imageBase64
-    });
-  }
-
   handleInputChange(e) {
     var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    console.log(file);
+    btoa(file);
     var pattern = /image-*/;
     var reader = new FileReader();
     if (!file.type.match(pattern)) {
-      alert('invalid format');
-      return;
+      this.UtilsService.flashMessage('error', 'Le format du fichier ne correspond pas. Veuillez sélectionner une image.');
+      // this.myFile.nativeElement.value = '';
+
+      return false;
+    } else {
+      if (file.size > 10000) {
+        this.UtilsService.flashMessage('error', 'L\'image sélectionnée est trop lourde.');
+        // this.myFile.nativeElement.value = '';
+
+        return false;
+      }
     }
     reader.onload = this._handleReaderLoaded.bind(this);
     reader.readAsDataURL(file);
@@ -49,7 +53,16 @@ export class CreateGamePageComponent implements OnInit {
   _handleReaderLoaded(e) {
     let reader = e.target;
     this.imageBase64 = reader.result;
-    console.log(this.imageBase64);
+  }
+
+  private initForm = () => {
+    this.form = this.FormBuilder.group({
+      name: [ undefined, Validators.required ],
+      platforms: this.FormBuilder.array([this.createPlatform()]),
+      modes: this.FormBuilder.array([this.createMode()]),
+      rules: this.FormBuilder.array([this.createRule()]),
+      image: this.imageBase64
+    });
   }
 
   createPlatform = () => {
@@ -97,7 +110,7 @@ export class CreateGamePageComponent implements OnInit {
   }
 
   public createGame = () => {
-    this.GameService.newGame( this.form.value.name, this.form.value.platforms, this.form.value.modes, this.form.value.rules, this.form.value.image )
+    this.GameService.newGame( this.form.value.name, this.form.value.platforms, this.form.value.modes, this.form.value.rules, this.imageBase64 )
     .then( apiResponse => {
       console.log(apiResponse);
       this.Router.navigate([ '/game/fiche/', apiResponse.data._id ]);
